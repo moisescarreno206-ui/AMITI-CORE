5b8fcdcdf1d43984047b04f4d8f7573d1ecc3e56
moisescarreno206-ui <moisescarreno206@gmail.com>
2026-06-07 13:35:44 -0400

    Create núcleo4_seguridad.py

---
+++ nucleos/nucleos/núcleo4_seguridad.py
@@ -0,0 +1,13 @@
+# Archivo: nucleos/núcleo4_seguridad.py
+
+def verificar_acceso():
+    """
+    Núcleo 4 Seguridad.
+    Verifica que la petición sea legítima y filtra amenazas.
+    """
+    return {
+        "status": "activo",
+        "modulo": "Seguridad",
+        "mensaje": "El núcleo 4 de seguridad está operando correctamente."
+    }
+  
