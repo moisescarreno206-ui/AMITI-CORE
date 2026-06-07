d44bbfd9630c9cb7f40773e22c622c849c04d866
moisescarreno206-ui <moisescarreno206@gmail.com>
2026-06-07 09:20:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+import sys
+import os
+# Añade la carpeta actual al path de Python
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from nucleos.n01_orquestador import app
 from nucleos.n05_activador import iniciar_activador
 
-# Inicia el corazón de AMITI justo al arrancar
 iniciar_activador()
 
 if __name__ == "__main__":
