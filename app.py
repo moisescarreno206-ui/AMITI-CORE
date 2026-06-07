20c903d9277c62fc5dd07272465b3ad6eda58a63
moisescarreno206-ui <moisescarreno206@gmail.com>
2026-06-07 10:06:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 import sys
 import os
-
-# Esto fuerza a Python a incluir la carpeta actual en su lista de búsqueda
 sys.path.append(os.getcwd())
 
 from nucleos.n01_orquestador import app
 from nucleos.n05_activador import iniciar_activador
 
-# Iniciamos el activador
 iniciar_activador()
 
-# El servidor Gunicorn usará esta variable 'app'
 if __name__ == "__main__":
     app.run()
     
