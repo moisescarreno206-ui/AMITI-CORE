8238941c5f6f76f08ec5e46777bf65eb3815a4a1
moisescarreno206-ui <moisescarreno206@gmail.com>
2026-06-07 10:07:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ sys.path.append(os.getcwd())
 from nucleos.n01_orquestador import app
 from nucleos.n05_activador import iniciar_activador
 
+# Iniciamos el activador
 iniciar_activador()
 
 if __name__ == "__main__":
