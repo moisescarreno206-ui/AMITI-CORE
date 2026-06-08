bab99f70cd357df1a4e3e7e165191c8f7d25449b
moisescarreno206-ui <moisescarreno206@gmail.com>
2026-06-08 11:43:23 -0400

    Update n50_ejecutor.py

--- nucleos/n50_ejecutor.py
+++ nucleos/n50_ejecutor.py
@@ -1,6 +1,9 @@
 # nucleos/n50_ejecutor.py
+from nucleos.n46_explorador_externo import analizar_texto
 
 def ejecutar_accion(comando, tipo):
+    if tipo == "analisis":
+        return analizar_texto(comando)
     return f"Sistema ejecutando {tipo}: {comando}"
 
-print("Núcleo 50 cargado exitosamente.")
+print("Núcleo 50 y Núcleo 46 cargados exitosamente.")
