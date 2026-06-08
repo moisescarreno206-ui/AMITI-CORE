a32a035df8fa18fc2b55f8150d2e626471f4f997
moisescarreno206-ui <moisescarreno206@gmail.com>
2026-06-08 10:12:25 -0400

    Update n50_ejecutor.py

--- nucleos/n50_ejecutor.py
+++ nucleos/n50_ejecutor.py
@@ -1,16 +1,3 @@
-from nucleos.n44_calculo_logico import procesar_calculo
-from nucleos.n48_arquitecto_autonomo import procesar_comando_arquitecto
-from nucleos.n03_notificador import enviar_alerta
-from nucleos.n45_memoria import guardar_memoria, leer_memoria
-from nucleos.n46_analizador_sentimental import analizar_texto
-from nucleos.n49_vigilante import vigilar_sistema
-
-def ejecutar_accion(comando, tipo):
-    if tipo == "calculo":
-        return procesar_calculo(comando)
-    elif tipo == "arquitecto":
-        return procesar_comando_arquitecto(comando)
-    elif tipo == "seguridad":
-        return enviar_alerta(comando)
-    return "Accion no definida"
-    
+# nucleos/n50_ejecutor.py
+# Debe coincidir exactamente con el nombre del archivo en la carpeta
+from nucleos.n46_explorador_externo import analizar_texto
